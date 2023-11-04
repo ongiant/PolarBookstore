756aa355cff9d849640ef92ab0095fffdc68f499
FlyFlame <ariesgenesis@outlook.com>
2023-11-04 23:07:55 +0800

    finish chapter 10.3

--- chapter10/dispatcher-service/src/test/java/com/polarbookshop/dispatcherservice/DispatchingFunctionsIntegrationTests.java
+++ chapter10/dispatcher-service/src/test/java/com/polarbookshop/dispatcherservice/DispatchingFunctionsIntegrationTests.java
@@ -5,7 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.function.context.FunctionCatalog;
 import org.springframework.cloud.function.context.test.FunctionalSpringBootTest;
 import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 import java.util.function.Function;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @FunctionalSpringBootTest
 public class DispatchingFunctionsIntegrationTests {
@@ -14,8 +17,30 @@ public class DispatchingFunctionsIntegrationTests {
     private FunctionCatalog catalog;
 
     @Test
+    void pack(){
+        Function<OrderAcceptedMessage, Long> pack = catalog.lookup(Function.class, "pack");
+        long orderId = 123;
+
+        assertThat(pack.apply(new OrderAcceptedMessage(orderId))).isEqualTo(orderId);
+    }
+
+    @Test
+    void label(){
+        Function<Flux<Long>, Flux<OrderDispatchedMessage>> label = catalog.lookup(Function.class, "label");
+        Flux<Long> orderIdFlux = Flux.just(122L);
+
+        StepVerifier.create(label.apply(orderIdFlux))
+            .expectNextMatches(labeledOrder -> labeledOrder.equals(new OrderDispatchedMessage(122L)))
+            .verifyComplete();
+    }
+
+    @Test
     void packAndLabelOrder(){
-        Function<OrderAcceptedMessage, Flux<OrderDispatchedMessage>> packAndLabel = catalog.lookup(Function.class, "pack | label");
+        Function<OrderAcceptedMessage, Flux<OrderDispatchedMessage>> packAndLabel = catalog.lookup(Function.class, "pack|label");
         long orderId = 121;
+
+        StepVerifier.create(packAndLabel.apply(new OrderAcceptedMessage(orderId)))
+            .expectNextMatches(dispatchedOrder -> dispatchedOrder.equals(new OrderDispatchedMessage(orderId)))
+            .verifyComplete();
     }
 }
