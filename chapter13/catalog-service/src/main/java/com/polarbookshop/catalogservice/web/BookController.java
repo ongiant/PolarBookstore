38e542de92d030d2637480d539c64939fdbfe8c7
FlyFlame <ariesgenesis@outlook.com>
2024-01-15 23:57:34 +0800

    finish chapter 13.2

--- chapter13/catalog-service/src/main/java/com/polarbookshop/catalogservice/web/BookController.java
+++ chapter13/catalog-service/src/main/java/com/polarbookshop/catalogservice/web/BookController.java
@@ -5,6 +5,8 @@ import javax.validation.Valid;
 import com.polarbookshop.catalogservice.domain.Book;
 import com.polarbookshop.catalogservice.domain.BookService;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("books")
 public class BookController {
+    private static final Logger log = LoggerFactory.getLogger(BookController.class);
 
     private final BookService bookService;
 
